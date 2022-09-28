b1ecc4184cf6dc7efc0c5f234f6f16e1aa600893
kubilaybulduk <80199382+kubilaybulduk@users.noreply.github.com>
2022-09-28 22:38:13 +0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 11;
+		String mesaj = "Öğrenci sayısı : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}

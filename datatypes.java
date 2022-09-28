b57f50cf28cdb31f7ba0aff86c56b0b1ed94c8db
kubilaybulduk <80199382+kubilaybulduk@users.noreply.github.com>
2022-09-28 23:36:10 +0300

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+	
+		int sayi = 10;
+		sayi = 12;
+		
+		double sayilar = 12.5;
+	
+		char karakter = 'A';
+		
+	}
+	
+}
